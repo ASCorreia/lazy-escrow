473712a39763773728b82e5fb7ea62615e9062a4
AndreiaCanadas <canadas90@gmail.com>
2025-05-14 00:51:03 +0100

    Added take method

--- programs/lazy-escrow/src/instructions/take.rs
+++ programs/lazy-escrow/src/instructions/take.rs
@@ -2,7 +2,7 @@ pub use anchor_lang::prelude::*;
 pub use anchor_lang::accounts::lazy_account::LazyAccount;
 pub use anchor_spl::{
     associated_token::AssociatedToken, 
-    token::{Mint, Token, TokenAccount, Transfer, transfer},
+    token::{Mint, Token, TokenAccount, Transfer, transfer, CloseAccount, close_account},
 };
 
 use crate::state::{Escrow, LazyEscrow};
@@ -21,6 +21,12 @@ pub struct Taker<'info>{
         associated_token::authority = escrow,
     )]
     pub vault_token_a: Account<'info, TokenAccount>,
+    #[account(
+        mut,
+        associated_token::mint = token_a,
+        associated_token::authority = maker,
+    )]
+    pub ata_maker_token_a: Account<'info, TokenAccount>,
     #[account(
         init_if_needed,
         payer = maker,
@@ -43,7 +49,9 @@ pub struct Taker<'info>{
     pub ata_taker_token_b: Account<'info, TokenAccount>,
     #[account(
         mut,
-        //has_one = 
+        close = maker,
+        seeds = [b"escrow", maker.key().as_ref()],
+        bump = *(escrow.load_bump()?),
     )]
     pub escrow: LazyAccount<'info, Escrow>,
     pub token_program: Program<'info, Token>,
@@ -55,9 +63,67 @@ impl<'info> Taker<'info> {
 
     pub fn transfer_token_b(&mut self) -> Result<()> {
 
-        //let cpi_program = self.
+        let cpi_program = self.token_program.to_account_info();
 
-        //let cpi_accounts = 
+        let cpi_accounts = Transfer {
+            from: self.ata_taker_token_b.to_account_info(),
+            to: self.ata_maker_token_b.to_account_info(),
+            authority: self.taker.to_account_info(),
+        };
+
+        let cpi_ctx = CpiContext::new(cpi_program, cpi_accounts);
+
+        let amount_b = self.escrow.load_amount_b()?;
+
+        transfer(cpi_ctx, *amount_b)?;
+
+        Ok(())
+    }
+
+    pub fn transfer_token_a(&mut self) -> Result<()> {
+
+        let cpi_program = self.token_program.to_account_info();
+
+        let cpi_accounts = Transfer {
+            from: self.vault_token_a.to_account_info(),
+            to: self.ata_maker_token_a.to_account_info(),
+            authority: self.escrow.to_account_info(),
+        };
+
+        let signer_seeds: [&[&[u8]]; 1] = [&[
+            b"escrow",
+            self.maker.to_account_info().key.as_ref(),
+            &[*(self.escrow.load_bump()?)],
+        ]];  
+
+        let cpi_ctx = CpiContext::new_with_signer(cpi_program, cpi_accounts, &signer_seeds);        
+
+        let amount_a = self.escrow.load_amount_a()?;
+
+        transfer(cpi_ctx, *amount_a)?;
+
+        Ok(())
+    }
+
+    pub fn close_vault(&mut self) -> Result<()> {
+
+        let cpi_program = self.token_program.to_account_info();
+
+        let cpi_accounts = CloseAccount {
+            account: self.vault_token_a.to_account_info(),
+            destination: self.maker.to_account_info(),
+            authority: self.escrow.to_account_info(),
+        };
+
+        let signer_seeds: [&[&[u8]]; 1] = [&[
+            b"escrow",
+            self.maker.to_account_info().key.as_ref(),
+            &[*(self.escrow.load_bump()?)],
+        ]];
+
+        let cpi_ctx = CpiContext::new_with_signer(cpi_program, cpi_accounts, &signer_seeds);
+
+        close_account(cpi_ctx)?;
 
         Ok(())
     }
--- programs/lazy-escrow/src/lib.rs
+++ programs/lazy-escrow/src/lib.rs
@@ -5,19 +5,21 @@ declare_id!("G12PpJUib62fdSGLveZxVTZnr9wnmF2bXsfY7TFPDAxX");
 mod state;
 mod instructions;
 
-use state::*;
 use instructions::*;
 
 #[program]
 pub mod lazy_escrow {
     use super::*;
 
-    pub fn make(ctx: Context<Maker>) -> Result<()> {
-        
-        //let mut my_escrow = ctx.accounts.escrow.load_mut()?;
+    pub fn make(ctx: Context<Maker>, amount_a: u64, amount_b: u64) -> Result<()> {
+        ctx.accounts.init_escrow(amount_a, amount_b, &ctx.bumps)?;
+        ctx.accounts.transfer_token_a()
+    }
 
-
-        Ok(())
+    pub fn take(ctx: Context<Taker>) -> Result<()> {
+        ctx.accounts.transfer_token_b()?;
+        ctx.accounts.transfer_token_a()?;
+        ctx.accounts.close_vault()
     }
 
     pub fn refund(ctx: Context<Refund>) -> Result<()> {
