765b54387e8ebccbe26a9f64b02189763a037ea0
ASCorreia <36857753+ASCorreia@users.noreply.github.com>
2025-05-24 19:27:02 -0400

    minor changes

--- programs/lazy-escrow/src/state/escrow.rs
+++ programs/lazy-escrow/src/state/escrow.rs
@@ -11,5 +11,5 @@ pub struct Escrow {
 }
 
 impl Space for Escrow {
-    const INIT_SPACE: usize = 1 + 32 + 32 + 32 + 8 + 8 + 1;
+    const INIT_SPACE: usize = 1 + (32 * 3) + (8 * 2) + 1; //1 discriminator (1 byte), 3 * pubkeys (32 bytes each), 2 * u64 (8 bytes each), 1 bump (1 byte)
 }
